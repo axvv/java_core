a24a2d50a79fca4c91ef0ab580f0d7711875c9b0
axvv <102459988+axvv@users.noreply.github.com>
2023-04-17 21:06:44 +0400

    Delete src/main/java directory

--- src/main/java/lesson2/MyArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package lesson2;
-
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(int i, int j) {
-        super(String.format("в ячейке %d-%d лежит символ или текст вместо числа", i, j));
-
-    }
-}
